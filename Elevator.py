e1d17fac6240f18b316b139bc17dfdd65f9f3e7e
Mahshid Marbouti <mahshid.marbouti@gmail.com>
2018-05-27 14:34:37 -0600

    Added Test Cases using unittest.

--- Elevator.py
+++ Elevator.py
@@ -1,10 +1,18 @@
 class Elevator:
     def __init__(self, currentFloor, doorStatus,  minFloor, maxFloor):
-        self.currentFloor = currentFloor
-        self.doorStatus = doorStatus
         self.maxFloor= maxFloor
         self.minFloor= minFloor
-
+        self._check_floor(currentFloor)
+        self.currentFloor = currentFloor
+        self._check_door_status(doorStatus)
+        self.doorStatus = doorStatus
+        
+    def _check_floor(self, currentFloor):
+        if currentFloor > self.maxFloor or currentFloor < self.minFloor:
+            raise ValueError("current floor should be between {0} and {1}".format(self.minFloor, self.maxFloor))
+    def _check_door_status(self, doorStatus):
+        if doorStatus!="C" and doorStatus!="O":
+            raise ValueError("Invalid Value for Door Status")
     def door_open(self, doorStatus):
         self.doorStatus= "O"
     def door_close(self, doorStatus):
@@ -25,15 +33,15 @@ class Elevator:
         """
         print("enter {0} , {1}".format(self.minFloor, self.maxFloor))
         self.door_close(self.doorStatus)
-        if requestedFloor <= self.maxFloor and requestedFloor>=self.minFloor:
-            if requestedFloor>= self.currentFloor:
-                step = 1
-            else:
-                step = -1
-            for item in range(self.currentFloor, requestedFloor, step):
-                print("floor Number {0}".format(item))
-            print("destination floor Number {0}".format(requestedFloor))
-            self.currentFloor= requestedFloor
+        self._check_floor(requestedFloor)
+        if requestedFloor>= self.currentFloor:
+            step = 1
+        else:
+            step = -1
+        for item in range(self.currentFloor, requestedFloor, step):
+            print("floor Number {0}".format(item))
+        print("destination floor Number {0}".format(requestedFloor))
+        self.currentFloor= requestedFloor
 
 def main():
     sample_elevator = Elevator(1, "C", -4, 14)
@@ -41,5 +49,5 @@ def main():
     sample_elevator.traverse(40)
     sample_elevator.traverse(-2)
 
-main()
+#main()
     
